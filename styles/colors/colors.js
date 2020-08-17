5f6f89590a97aed7c7320901b15be75fcea3023d
Shawn <shawn@karboncard.com>
2020-08-17 14:03:14 +0800

    add style

---
+++ styles/colors/colors.js
@@ -0,0 +1,25 @@
+/* 通用色 */
+export const THEME_COLORS = {
+  primary: '#4A8CF9', // 主色
+  secondary: '#0C3476', // 次主色
+  success:'#52C41B', // 成功
+  info: '#FAAD14', // 提示
+  danger: '#F4222D', // 错误，警告
+  white: '#FFFFFF', // 纯白色
+  dark: '#0c0c0c' // 黑色
+}
+
+/* 用于文字的颜色 */
+export const DARK_COLORS = {
+  dark85: '#262626', // 85%
+  dark65: '#595959', // 65%
+  dark45: '#8C8C8C', // 45%
+  dark35: '#AEAEAE' // 35%
+}
+
+/* 用于背景和边框色 */
+export const GRAY_COLORS = {
+  gray: '#E2E5ED', // 灰
+  grayLight: '#EAEDF3', // 浅灰
+  grayLighter: '#F9F9FA' // 超浅灰
+}
